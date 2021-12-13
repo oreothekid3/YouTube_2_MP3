89b8ff991eab53e2cc51f4a5510af71deeaf6274
Marquez <82126996+oreothekid3@users.noreply.github.com>
2021-12-13 11:44:00 -0800

    Changing the extension
    Added code to change the extension of the audio downloads to mp3.

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 ####last thing i left off at was trying to convert downloaded audio file to mp3 extention 
 
 
-
-
-
+import os
 #framework to create a GUI
 from tkinter import *
 from tkinter import ttk
-
 #library for downloading videos from youtube
 from pytube import YouTube
 
@@ -41,7 +38,10 @@ def Video_Downloader():
 def Audio_Downloader():
     url = YouTube(str(link.get()))
     audio = url.streams.filter(only_audio=True).first()
-    audio.download()
+    downloaded_audio = audio.download()
+    base, ext = os.path.splitext(downloaded_audio)
+    new_file = base + '.mp3'
+    os.rename(downloaded_audio, new_file)
     Label(main, text = 'AUDIO DOWNLOADED', font = 'arial 15').place(x = 180, y = 210)
 
 Button(main, text = 'VIDEO DOWNLOAD', font = 'arial 15 bold', bg = '#FF0000', padx = 2, command = Video_Downloader).place(x = 180, y = 160)
